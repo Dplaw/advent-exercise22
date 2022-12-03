d83b9febd3cbd93fb645b5e9615116aed4b300b8
Jakub Szwajka <szwajkajakub@gmail.com>
2022-12-03 13:25:02 +0100

    Update part1.py
    small refactor ;)

--- part1.py
+++ part1.py
@@ -1,19 +1,23 @@
 def main():
     data = get_data()
-    result = elf_most_calories(data)
-    return result
+    result = get_most_caloric_pack(data)
+    print(result)
 
-
+    
 def get_data():
     with open('adv.txt', mode='r') as file:
         data = file.read()
     return [grp.split("\n") for grp in data.split("\n\n")]
     
 
-def elf_most_calories(calories_pack):
-    return max([sum([int(s) for s in cal]) for cal in calories_pack])
+def get_most_caloric_pack(packs):
+    calories_per_pack = [
+        sum([int(s) for cal in pack]) 
+        for pack in packs
+    ]
+    return max(calories_per_pack)
 
 
 if __name__ == "__main__":
-    m = main()
-    print(m)
+    main()
+    
